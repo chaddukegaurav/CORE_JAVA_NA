4ec434e47c8d43b1ecddc6f966ac02ac5642425d
Gaurav Chadduke <csea524gauravchadduke@gmail.com>
2023-07-26 09:50:09 +0530

    added new feature

---
+++ src/com/corejava/assgn1/Main.java
@@ -0,0 +1,10 @@
+package com.corejava.assgn1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I have become death, destroyer of world");
+	}
+
+}
